7fbce37441417ddc13fea1fa3e526c1ed6acbb26
Mathis Börner <mathis.boerner@googlemail.com>
2023-01-21 14:04:24 +0100

    change to 10 repeats

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 query_limits=("5" "20" "50" "100" "200")
 query_files=("data/illumina_reads_40.fasta.gz" "data/illumina_reads_60.fasta.gz" "data/illumina_reads_80.fasta.gz" "data/illumina_reads_100.fasta.gz")
-repeats=1
+repeats=10
 for lim in ${query_limits[@]}; do
     for query in ${query_files[@]}; do
         for i in $(seq $repeats); do
